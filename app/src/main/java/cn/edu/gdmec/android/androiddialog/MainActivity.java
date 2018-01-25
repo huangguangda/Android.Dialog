1661ba6abb2cf7476cde8f2899a1e08eeedeb624
huangguangda <2397923107@qq.com>
2018-01-25 19:55:07 +0800

    1

--- app/src/main/java/cn/edu/gdmec/android/androiddialog/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/androiddialog/MainActivity.java
@@ -1,7 +1,8 @@
 package cn.edu.gdmec.android.androiddialog;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView ( R.layout.activity_main );
 
         //创建对话框
-        new AlertDialog.Builder ( this ).setTitle("Dialog")
+        /*AlertDialog dialog=new AlertDialog.Builder ( this ).setTitle ( "Dialog" )
                 .setMessage ( "是否确定退出" )//设置提示信息
                 .setIcon ( R.mipmap.ic_launcher )//设置图标
-                .setPositiveButton ( "确定",null )//设置按钮
-                .setNegativeButton ( "取消",null )//设置按钮
-                .create ().show ();
+                .setPositiveButton ( "确定", null )//设置按钮
+                .setNegativeButton ( "取消", null )//设置按钮
+                .create ();
+
+        dialog.show ();*/
+
+        //单选对话框
+        AlertDialog dialog = new AlertDialog.Builder ( this ).setTitle ( "请选择性别" )
+                .setIcon ( R.mipmap.ic_launcher )//设置图标
+                .setPositiveButton ( "确定",null )//添加确定按钮
+                .setSingleChoiceItems ( new String[]{"男", "女"}, -1, new DialogInterface.OnClickListener () {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                } ).create ();
+
+        dialog.show ();
     }
 }
